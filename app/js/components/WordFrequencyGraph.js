6d649e262f82cfae5e0b7b2669fe9c0a48f3899e
Jason Poon <git@jasonpoon.ca>
2015-10-25 16:42:40 -0700

    add charts

--- app/js/components/WordFrequencyGraph.js
+++ app/js/components/WordFrequencyGraph.js
@@ -3,10 +3,10 @@
 import ClassNames       from 'classnames';
 import React            from 'react';
 import _                from 'lodash';
-import Chartist         from 'react-chartist';
-import createFragment   from 'react-addons-create-fragment';
+import {Bar as BarChart} from 'react-chartjs';
 
 const WordFrequencyGraph = React.createClass({
+
   _getWordFrequency(sentence) {
     var wordFrequencyTmp = {};
     var words = _.words(sentence);
@@ -25,33 +25,61 @@ const WordFrequencyGraph = React.createClass({
       wordFrequency.push({ word: key, count: value});
     });
 
-    return _.sortByOrder(wordFrequency, ['count', 'word'], ['asc', 'desc']);
+    return _.sortByOrder(wordFrequency, ['count', 'word'], ['desc', 'asc']);
+  },
+
+  getInitialState() {
+    return { shouldComponentUpdate: true };
+  },
+
+  componentDidMount() {
+    var self = this;
+    this.timer = setInterval(function() {
+      self.setState({shouldComponentUpdate: true})
+    }, 750);
+  },
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  },
+
+  shouldComponentUpdate(nextProps, nextState) {
+    var shouldComponentUpdate = false
+
+    if (nextProps.transcript != this.props.transcript) {
+      shouldComponentUpdate = this.state.shouldComponentUpdate;
+      this.state.shouldComponentUpdate = false;
+    }
+
+    return shouldComponentUpdate;
   },
 
   render() {
     var wordFrequency = this._getWordFrequency(this.props.transcript);
 
-    console.log(wordFrequency);
-    var data = {
+    var chartData = {
       labels: _.pluck(wordFrequency, "word"),
-      series: [_.pluck(wordFrequency, "count")],
+      datasets: [
+          {
+              label: "words",
+              fillColor: "rgba(220,220,220,0.5)",
+              strokeColor: "rgba(220,220,220,0.8)",
+              highlightFill: "rgba(220,220,220,0.75)",
+              highlightStroke: "rgba(220,220,220,1)",
+              data: _.pluck(wordFrequency, "count")
+          },
+      ]
     };
 
-    var options = {
-      horizontalBars: true,
-      axisX: {
-        labelInterpolationFnc: function(value) {
-          return value % 1 === 0 ? value : null;
-        }
-      },
-      axisY: {
-        showGrid: false,
-      }
-    };
+    var chartOptions = {
+      responsive: true,
+      scaleShowVerticalLines: false,
+    }
 
     return (
       <div>
-        <Chartist data={data} options={options} type='Bar' />
+        <h2>Word Frequency</h2>
+         <BarChart data={chartData} options={chartOptions} redraw />
       </div>
     );
   }
