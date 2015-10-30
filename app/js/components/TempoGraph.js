34331e65ba16925078c8a42d14cc97d2c035e6f0
Jason Poon <git@jasonpoon.ca>
2015-10-29 18:02:07 -0700

    tempo line graph

--- app/js/components/TempoGraph.js
+++ app/js/components/TempoGraph.js
@@ -1,18 +1,18 @@
 'use strict';
 
-import ClassNames       from 'classnames';
-import React            from 'react';
-import _                from 'lodash';
+import ClassNames           from 'classnames';
+import React                from 'react';
+import _                    from 'lodash';
+import {Line as LineChart}  from 'react-chartjs';
+
 
 const TempoGraph = React.createClass({
-  sampleWindowInSec: 10,
+  sampleWindowInSec: 2,
 
   getInitialState() {
     return {
       windowTempo: [],
-      wordCount: [],
-      sampleRate: [],
-      windowStartTime: new Date(),
+      windowStartTime: [],
       windowStartWordCount: 0,
     }
   },
@@ -23,14 +23,14 @@ const TempoGraph = React.createClass({
     if (this.state.windowStartWordCount == 0) {
       // start sampling window
       this.setState({
-        windowStartTime: new Date(),
+        windowStartTime: this.state.windowStartTime.concat([new Date().getTime() / 1000]),
         windowStartWordCount: _.words(nextProps.transcript).length,
       });
 
       return;
     }
 
-    var elapsedTimeInSec = Math.floor((new Date().getTime() - this.state.windowStartTime.getTime()) / 1000)
+    var elapsedTimeInSec = Math.floor(new Date().getTime()/1000 - _.last(this.state.windowStartTime));
 
     if (elapsedTimeInSec < this.sampleWindowInSec ) {
       // still sampling
@@ -44,21 +44,40 @@ const TempoGraph = React.createClass({
 
     this.setState({
       windowTempo: this.state.windowTempo.concat([tempo]),
-      windowStartTime: new Date(),
+      windowStartTime: this.state.windowStartTime.concat([new Date().getTime() / 1000]),
       windowStartWordCount: wordCntTotal,
     });
 
   },
 
   render() {
+    var first = _.first(this.state.windowStartTime);
+
+    var chartData = {
+      labels: _.slice(this.state.windowStartTime.map(t => { return Math.round(t - first); }), 1, -1),
+      datasets: [
+          {
+              labels: "tempo",
+              fillColor: "rgba(220,220,220,0.5)",
+              strokeColor: "rgba(220,220,220,0.8)",
+              highlightFill: "rgba(220,220,220,0.75)",
+              highlightStroke: "rgba(220,220,220,1)",
+              data: this.state.windowTempo,
+          },
+      ]
+    };
+
+    var chartOptions = {
+      responsive: true,
+      scaleShowVerticalLines: false,
+    }
+
     return (
       <div>
         <h2>Tempo</h2>
-        <ul className="Uli">
-        {this.state.windowTempo.map(function(value) {
-            return <li>{value}</li>
-        })}
-        </ul>
+          { this.state.windowTempo.length > 0 ?
+           <LineChart data={chartData} options={chartOptions} redraw /> : null
+          }
       </div>
     );
   }
